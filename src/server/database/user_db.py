f0e9e756cf6c5e0920dee2ff2ff0ebcf6eb292af
Kiet-2004 <kiet23022004@gmail.com>
2025-05-27 15:05:26 +0700

    Modified the user_db so that it can return username for later query handling

--- src/server/database/user_db.py
+++ src/server/database/user_db.py
@@ -2,9 +2,6 @@ import csv
 from pydantic import BaseModel
 import os
 from server.config import settings
-class User(BaseModel):
-    user_name: str
-    password: str
 
 class UserDB:
     def __init__(self, data_file = "user.csv") -> None:
@@ -12,21 +9,21 @@ class UserDB:
         self.data_file = os.path.join(settings.STORAGE_FOLDER, data_file)
         with open(self.data_file, 'r') as file:
             reader = csv.DictReader(file)
-            self.data = [User(user_name=row['user_name'], password=row['password']) for row in reader]
+            self.data = [{"user_name": row['user_name'], "password": row['password']} for row in reader]
 
-    def get_user(self, user_name: str) -> User | None:
+    def get_user(self, user_name: str) -> str | None:
         for user in self.data:
-            if user.user_name == user_name:
-                return user
+            if user['user_name'] == user_name:
+                return user['user_name']
         return None
     
-    def add_user(self, user_name: str, password: str) -> User | None:
-        user = User(user_name=user_name, password=password)
+    def add_user(self, user_name: str, password: str) -> str | None:
+        user = {"user_name": user_name, "password": password}
         self.data.append(user)
         with open(self.data_file, 'a') as file:
             writer = csv.DictWriter(file, fieldnames=["user_name", "password"])
-            writer.writerow({"user_name": user.user_name, "password": user.password})
+            writer.writerow(user)
             file.close()
-        return user
+        return user['user_name']
 
 USER_DATABASE = UserDB()
