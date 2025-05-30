7511a89db79d284b974e28ad94fb11ab8dd68c19
Kiet-2004 <kiet23022004@gmail.com>
2025-05-30 09:48:08 +0700

    Added async for client-side and create some custom Exception for handling errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,2 +1,20 @@
 # API tests
-import dbapi2
+
+import asyncio
+from dbapi2.connect import connect
+
+async def main():
+    async with await connect(
+        url="http://127.0.0.1:8000",
+        username="string",
+        password="stringst",
+        db_name="CompanyDB"
+    ) as conn:
+        cursor = conn.cursor()
+        await cursor.execute("SELECT * FROM employees where id <= 10;")
+        result = cursor.fetchall()
+        print(result)
+
+# Run the async code
+if __name__ == "__main__":
+    asyncio.run(main())
